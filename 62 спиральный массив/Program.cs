67315a6fa0f61a86fef0b6cea0f21c58bb2dcd81
Aleksandr <aleksandr-s86>
2023-03-11 22:31:06 +0300

    спиральный массив

---
+++ 62 спиральный массив/Program.cs
@@ -0,0 +1,44 @@
+﻿/*62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
+int size = 4;
+int[,] array = new int [size,size];
+FillArray(array, size);
+PrintArray(array);
+
+void FillArray(int[,] inArray, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] < 10)
+            {
+                Console.Write("0" + inArray[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
